a617badbc387db291eb50a480ee83067041849b6
kihno <stistyle13@gmail.com>
2022-10-21 15:37:05 -0700

    Add user posts to user profile page

--- user-client/src/components/Profile.js
+++ user-client/src/components/Profile.js
@@ -4,23 +4,50 @@ import apis from '../api';
 
 const Profile = (props) => {
     const [user, setUser] = useState(null);
+    const [posts, setPosts] = useState([]);
+    const [hasPosts, setHasPosts] = useState(false);
     const { id } = useParams();
     
     useEffect(() => {
         apis.getUserById(id).then(res => {
             setUser(res.data);
         });
-
     }, [id]);
+
+    useEffect(() => {
+        apis.getAllPosts().then(res => {
+            res.data.map(post => {
+                console.log(id);
+                console.log(post.user._id);
+                if (post.user._id === id) {
+                    setPosts(prevPosts => [...prevPosts, post])
+                }
+            });
+        });
+    }, []);
+
+    useEffect(() => {
+        if (posts.length > 0) {
+            setHasPosts(true);
+        }
+    }, [posts]);
 
     return(
         <section className='profile'>
-            {!user ? 'No user information' : 
+            {!user ? <div>No user information</div> : 
                 <div className='user-username'>
                     <h2>Username:</h2>
                     <span>{user.username}</span>
                 </div>
             }
+            <div className='user-posts'>
+                <h2>Posts:</h2>
+                    <ul>
+                        {hasPosts && posts.map(post => {
+                        return <li><a href={'/posts/' + post._id}>{post.title}</a></li>
+                        })}
+                    </ul>
+            </div>
         </section>
     )
 }
