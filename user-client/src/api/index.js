11027bc46da50895bf2865d19eeccdc8b6554357
kihno <stistyle13@gmail.com>
2022-12-10 14:52:44 -0800

    Update server url

--- user-client/src/api/index.js
+++ user-client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://blog-h854.onrender.com',
+    baseURL: 'https://blog-server-bgu3.onrender.com',
 });
 
 export const getPostById = id => api.get(`/posts/${id}`);
